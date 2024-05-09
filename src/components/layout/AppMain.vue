b844e2259e83b61aa648b3841de18a3a835d181e
yusef02 <youssef.bouziane2402@gmail.com>
2024-05-09 17:13:29 +0200

    add axios request to fetch services

--- src/components/layout/AppMain.vue
+++ src/components/layout/AppMain.vue
@@ -5,40 +5,47 @@ import axios from "axios";
 import { api, store } from "../../store";
 
 export default {
-  data() {
-    return {
-      store,
-    };
-  },
+	data() {
+		return {
+			store,
+		};
+	},
 
-  components: { AppSearch, AppCard },
+	components: { AppSearch, AppCard },
 
-  methods: {
-    fetchSponsoredAppartment(endpoint = api.baseUrl + "appartments") {
-      axios.get(endpoint).then((res) => {
-        store.sponsoredAppartments = res.data;
-        console.log(store.sponsoredAppartments);
-      });
-    },
-  },
+	methods: {
+		fetchSponsoredAppartment(endpoint = api.baseUrl + "appartments") {
+			axios.get(endpoint).then((res) => {
+				store.sponsoredAppartments = res.data;
+				console.log(store.sponsoredAppartments);
+			});
+		},
+		fetchServices(endpoint = api.baseUrl + "services") {
+			axios.get(endpoint).then((res) => {
+				store.services = res.data;
+				console.log(store.services);
+			});
+		},
+	},
 
-  created() {
-    this.fetchSponsoredAppartment();
-  },
+	created() {
+		this.fetchSponsoredAppartment();
+		this.fetchServices();
+	},
 };
 </script>
 
 <template>
-  <AppSearch></AppSearch>
-  <main class="my-5">
-    <div class="container">
-      <div class="row g-4">
-        <div class="col-3" v-for="appartment in store.sponsoredAppartments">
-          <AppCard :appartment="appartment"></AppCard>
-        </div>
-      </div>
-    </div>
-  </main>
+	<AppSearch></AppSearch>
+	<main class="my-5">
+		<div class="container">
+			<div class="row g-4">
+				<div class="col-3" v-for="appartment in store.sponsoredAppartments">
+					<AppCard :appartment="appartment"></AppCard>
+				</div>
+			</div>
+		</div>
+	</main>
 </template>
 
 <style lang="scss" scoped></style>
