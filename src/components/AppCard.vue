7a91e797f8c370cc1ca0824264090ca722d4953b
yusef02 <youssef.bouziane2402@gmail.com>
2024-05-09 12:56:28 +0200

    good stuff 3

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,60 +1,59 @@
 <script>
-import axios from "axios";
-
 export default {
-  props: {
-    appartment: Object,
-  },
+	props: {
+		appartment: Object,
+	},
 };
 </script>
 
 <template>
-  <router-link
-    :to="{
-      name: 'appartmentDetails',
-      params: { appartmentSlug: appartment.slug },
-    }"
-  >
-    <div class="card h-100 my_card">
-      <img :src="appartment.imgUrl" class="card-img-top" alt="" />
-      <div class="card-body">
-        <h5 class="card-title pb-3">{{ appartment.title }}</h5>
-        <span class="card-text">{{ appartment.address }}</span>
-        <!-- <div class="mt-3"><strong>Stanze: </strong>{{ appartment.rooms > 1 ? appartment.rooms : 'Una stanza' }}</div>
+	<router-link
+		:to="{
+			name: 'appartmentDetails',
+			params: { appartmentSlug: appartment.slug },
+		}"
+		class="text-decoration-none"
+	>
+		<div class="card h-100 my_card">
+			<img :src="appartment.imgUrl" class="card-img-top" alt="" />
+			<div class="card-body">
+				<h5 class="card-title pb-3">{{ appartment.title }}</h5>
+				<span class="card-text">{{ appartment.address }}</span>
+				<!-- <div class="mt-3"><strong>Stanze: </strong>{{ appartment.rooms > 1 ? appartment.rooms : 'Una stanza' }}</div>
             <div class="mt-1"><strong>Metri quadri: </strong>{{ appartment.square_meters }}</div> -->
-        <div class="mt-4">
-          <strong>Host: </strong>{{ appartment.user.name }}
-        </div>
-        <div class="text-end"><strong>300€</strong> / notte</div>
-      </div>
-    </div>
-  </router-link>
+				<div class="mt-4">
+					<strong>Host: </strong>{{ appartment.user.name }}
+				</div>
+				<div class="text-end"><strong>300€</strong> / notte</div>
+			</div>
+		</div>
+	</router-link>
 </template>
 
 <style lang="scss">
 span {
-  opacity: 0.6;
+	opacity: 0.6;
 }
 
 .card-title {
-  border-bottom: 1px solid gray;
+	border-bottom: 1px solid gray;
 }
 
 .my_card {
-  transition: transform 0.5s;
-  cursor: pointer;
-  position: relative;
+	transition: transform 0.5s;
+	cursor: pointer;
+	position: relative;
 
-  &:hover {
-    transform: scale(1.1);
-  }
+	&:hover {
+		transform: scale(1.1);
+	}
 }
 
 .card-title {
-  height: 50px;
-  text-align: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+	height: 50px;
+	text-align: center;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 }
 </style>
