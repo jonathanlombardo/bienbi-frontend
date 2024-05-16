44e59704da7a408ef2613cfc31990788c284b009
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-16 20:32:14 +0200

    fix AppCard

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -7,22 +7,21 @@ export default {
 </script>
 
 <template>
-  <div >
-    
-      <div class="card h-100 my_card">
-        <div v-if="appartment.isSponsored" class="sponsorLabel"><i class="fa-solid fa-crown"></i> sponsorizzato</div>
-        <img :src="appartment.imgUrl" class="card-img-top" alt="" />
-        <div class="card-body">
-          <h5 class="card-title pb-3">{{ appartment.title }}</h5>
-          <span class="card-text">{{ appartment.address }}</span>
-          <div class="mt-2" v-if="appartment.distance"><strong>Distanza: </strong>{{ appartment.distance }} m</div>
-          <div>
-            <span v-for="service in appartment.services" class="d-inline-block mt-2 me-2"><i :class="service.faIconClass"></i></span>
-          </div>
-          <div class="mt-4"><strong>Host: </strong>{{ appartment.user.name }}</div>
-          <div class="text-end"><strong>300€</strong> / notte</div>
+  <div class="h-100">
+    <div class="card h-100 my_card">
+      <div v-if="appartment.isSponsored" class="sponsorLabel"><i class="fa-solid fa-crown"></i> sponsorizzato</div>
+      <img :src="appartment.imgUrl" class="card-img-top" alt="" />
+      <div class="card-body d-flex flex-column">
+        <h5 class="card-title pb-3">{{ appartment.title }}</h5>
+        <span class="card-text">{{ appartment.address }}</span>
+        <div class="mt-2" v-if="appartment.distance"><strong>Distanza: </strong>{{ appartment.distance }} m</div>
+        <div class="mb-4">
+          <span v-for="service in appartment.services" class="d-inline-block mt-2 me-2"><i :class="service.faIconClass"></i></span>
         </div>
+        <div class="mt-auto"><strong>Host: </strong>{{ appartment.user.name }}</div>
+        <div class="text-end"><strong>300€</strong> / notte</div>
       </div>
+    </div>
   </div>
 </template>
 
